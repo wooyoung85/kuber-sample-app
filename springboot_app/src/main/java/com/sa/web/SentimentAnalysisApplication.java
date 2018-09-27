baab250a4ea1c28c5ef604793c61b761fc06a8d9
우영 <wooyoung@uyeong-ui-MacBook-Pro.local>
2018-09-27 20:36:32 +0900

    spring boot app initial commit

---
+++ springboot_app/src/main/java/com/sa/web/SentimentAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.sa.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentAnalysisApplication.class, args);
+	}
+}
---
+++ springboot_app/src/main/java/com/sa/web/SentimentController.java
@@ -0,0 +1,29 @@
+package com.sa.web;
+
+import com.sa.web.dto.SentenceDto;
+import com.sa.web.dto.SentimentDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class SentimentController {
+
+    @Value("${sa.logic.api.url}")
+    private String saLogicApiUrl;
+
+    @PostMapping("/sentiment")
+    public SentimentDto sentimentAnalysis(@RequestBody SentenceDto sentenceDto) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.getForObject(saLogicApiUrl + "/api/analysis?sentence="+ sentenceDto.getSentence(), SentimentDto.class);
+    }
+
+    @GetMapping("/testHealth")
+    public String testHealth() {
+        return "Health Check";
+    }
+}
+
+
